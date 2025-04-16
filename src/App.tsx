98e1ba62bae84827cda5847633b362f8f97b6772
Nick Matantsev <nick.matantsev@gmail.com>
2025-04-15 22:05:07 -0400

    Add basic timer state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
+import { DateTime } from "luxon";
+
+interface TimerState {
+  start: DateTime;
+  current: DateTime;
+}
+
+const Timer: React.FC = () => {
+  const [timerState, setTimerState] = useState<TimerState | null>(null);
+
+  const start = timerState?.start;
+  useEffect(() => {
+    if (!start) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimerState(
+        (prevState) =>
+          prevState && {
+            ...prevState,
+            current: DateTime.now(),
+          },
+      );
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [start]);
+
+  function startTimer() {
+    setTimerState({
+      start: DateTime.now(),
+      current: DateTime.now(),
+    });
+  }
+
+  return (
+    <div>
+      {timerState ? (
+        <div>
+          {timerState.start.toFormat("hh:mm:ss")}
+          {timerState.current.toFormat("hh:mm:ss")}
+        </div>
+      ) : (
+        <div>--</div>
+      )}
+      <button
+        className="btn btn-primary"
+        type="button"
+        onClick={() => {
+          startTimer();
+        }}
+      >
+        Start
+      </button>
+    </div>
+  );
+};
 
 function App() {
-  return <div>TBD</div>;
+  return (
+    <div>
+      <Timer />
+    </div>
+  );
 }
 
 export default App;
