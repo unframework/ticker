a043ed5bf7991bf1bb4d7ad33f1617c407db7728
Nick Matantsev <nick.matantsev@gmail.com>
2025-04-16 00:55:17 -0400

    Add work cycle structure logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import "./App.css";
 import { DateTime } from "luxon";
@@ -7,6 +7,11 @@ interface TimerState {
   start: DateTime;
   current: DateTime;
 }
+
+const preDelay = 10; // in seconds
+const cycleWork = 20; // in seconds
+const cycleRest = 10; // in seconds
+const cycleCount = 8;
 
 const Timer: React.FC = () => {
   const [timerState, setTimerState] = useState<TimerState | null>(null);
@@ -39,25 +44,100 @@ const Timer: React.FC = () => {
     });
   }
 
+  const seq = useMemo(() => {
+    if (!timerState) {
+      return null;
+    }
+
+    const secondsDiff = Math.floor(
+      timerState.current.diff(timerState.start, "seconds").seconds,
+    );
+
+    if (secondsDiff < preDelay) {
+      return {
+        type: "preDelay" as const,
+        timeLeft: preDelay - secondsDiff,
+      };
+    }
+
+    const cycleTime = secondsDiff - preDelay;
+    const cycleIndex = Math.floor(cycleTime / (cycleWork + cycleRest));
+    if (cycleIndex >= cycleCount) {
+      return {
+        type: "done" as const,
+      };
+    }
+
+    const cycleStart = cycleIndex * (cycleWork + cycleRest);
+    const cycleElapsed = cycleTime - cycleStart;
+
+    if (cycleElapsed < cycleWork) {
+      return {
+        type: "work" as const,
+        cycleTime,
+        cycleIndex,
+        timeLeft: cycleWork - cycleElapsed,
+      };
+    }
+
+    const cycleRestElapsed = cycleElapsed - cycleWork;
+
+    return {
+      type: "rest" as const,
+      cycleTime,
+      cycleIndex,
+      timeLeft: cycleRest - cycleRestElapsed,
+    };
+  }, [timerState]);
+
   return (
     <div>
-      {timerState ? (
+      {seq ? (
         <div>
-          {timerState.start.toFormat("hh:mm:ss")}
-          {timerState.current.toFormat("hh:mm:ss")}
+          {seq.type === "preDelay" && (
+            <div>Get ready: {seq.timeLeft} seconds</div>
+          )}
+
+          {seq.type === "work" && (
+            <div>
+              Cycle {seq.cycleIndex + 1}/{cycleCount}: work {seq.timeLeft}{" "}
+              seconds
+            </div>
+          )}
+
+          {seq.type === "rest" && (
+            <div>
+              Cycle {seq.cycleIndex + 1}/{cycleCount}: rest {seq.timeLeft}{" "}
+              seconds
+            </div>
+          )}
+
+          {seq.type === "done" && <div>Done!</div>}
         </div>
       ) : (
         <div>--</div>
       )}
-      <button
-        className="btn btn-primary"
-        type="button"
-        onClick={() => {
-          startTimer();
-        }}
-      >
-        Start
-      </button>
+
+      <div className="flex gap-2">
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={() => {
+            startTimer();
+          }}
+        >
+          Start
+        </button>
+        <button
+          className="btn btn-error"
+          type="button"
+          onClick={() => {
+            setTimerState(null);
+          }}
+        >
+          Stop
+        </button>
+      </div>
     </div>
   );
 };
