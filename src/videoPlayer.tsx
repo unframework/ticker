81a9d8b118abf0e8f4838ca8fe2d6bb740a24e50
Nick Matantsev <nick.matantsev@gmail.com>
2025-06-09 14:38:34 -0400

    Tweak YT player lifecycle

--- src/videoPlayer.tsx
+++ src/videoPlayer.tsx
@@ -1,34 +1,45 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import clsx from "clsx";
 import YouTubePlayer from "youtube-player";
 
-async function initPlayer(container: HTMLDivElement) {
-  const player = YouTubePlayer(container);
-
-  player.cueVideoById("dQw4w9WgXcQ");
-  // player.playVideo();
+export interface VideoPlayerProps {
+  videoId: string;
 }
 
-export const VideoPlayer: React.FC = () => {
+export const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoId }) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const playerRef = useRef<ReturnType<typeof YouTubePlayer> | null>(null);
+  const [playerReady, setPlayerReady] = useState(false);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      initPlayer(containerRef.current!);
+      playerRef.current = YouTubePlayer(containerRef.current!);
+      setPlayerReady(true);
     }, 50);
 
-    // Debounce double-invocations of useEffect
+    // Debounce double-invocations of useEffect,
+    // and also destroy the player on unmount
     return () => {
       clearTimeout(timeoutId);
+      playerRef.current?.destroy();
     };
   }, []);
+
+  // Video cueing
+  useEffect(() => {
+    if (!playerReady) {
+      return;
+    }
+
+    playerRef.current?.cueVideoById(videoId);
+  }, [playerReady, videoId]);
 
   return (
     <div
       ref={containerRef}
       className={clsx(
         "w-96 h-56 flex items-center justify-center",
-        "bg-gray-200 rounded-sm overflow-hidden"
+        "bg-gray-200 rounded-sm overflow-hidden",
       )}
     >
       VideoPlayer
